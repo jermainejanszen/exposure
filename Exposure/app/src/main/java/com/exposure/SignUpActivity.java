2633571b40bc49ef22436f106ec5e0deddc7e14d
Ben Gane <bgane@live.com.au>
2020-10-05 15:51:50 +1100

    Implemented login persistence. I've left it as a comment for now so we can manually sign in each time we run the app

--- Exposure/app/src/main/java/com/exposure/SignUpActivity.java
+++ Exposure/app/src/main/java/com/exposure/SignUpActivity.java
@@ -31,6 +31,15 @@ public class SignUpActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        /**
+         * If the user is already logged in, navigate to the map activity page.
+         * I'm leaving this as a comment until we actually need to use it.
+         *
+         * if (null != mAuth.getCurrentUser()) {
+         *     startActivity(new Intent(this, MapActivity.class));
+         * }
+         */
+
         fullNameField = findViewById(R.id.full_name_field);
         emailField = findViewById(R.id.email_field);
         passwordField = findViewById(R.id.password_field);
