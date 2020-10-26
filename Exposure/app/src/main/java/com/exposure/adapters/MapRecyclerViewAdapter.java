f52437b78110bf445891f60a5153b97043e25c75
Holly Craig <holly.g.craig@gmail.com>
2020-10-26 18:47:02 +1100

    Upon clicking on user in map, takes you to their profile.

--- Exposure/app/src/main/java/com/exposure/adapters/MapRecyclerViewAdapter.java
+++ Exposure/app/src/main/java/com/exposure/adapters/MapRecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package com.exposure.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.exposure.R;
+import com.exposure.activities.ViewOtherProfileActivity;
 
 import java.util.List;
 
@@ -36,7 +40,15 @@ public class MapRecyclerViewAdapter extends RecyclerView.Adapter<MapRecyclerView
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         // holder.getProfileImage().setImageBitmap(data.get(position).getProfileImage());
         holder.getName().setText(data.get(position).getName());
-    }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(context, ViewOtherProfileActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+        }
 
     @Override
     public int getItemCount() {
