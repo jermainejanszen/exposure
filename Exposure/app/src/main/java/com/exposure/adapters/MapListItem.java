0725253708001d3d226295695405c67d95e3d641
Holly Craig <holly.g.craig@gmail.com>
2020-11-01 21:41:09 +1100

    MapListItem - Javadocs, Comments, Formatting

--- Exposure/app/src/main/java/com/exposure/adapters/MapListItem.java
+++ Exposure/app/src/main/java/com/exposure/adapters/MapListItem.java
@@ -13,16 +13,22 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.Serializable;
-
+/**
+ * Represents a user item in seen in the map fragment of the application
+ */
 public class MapListItem {
 
     private final String uid;
     private Bitmap profileImage;
     private String name;
-
     private final OnCompleteCallback completeCallback;
 
+    /**
+     * Constructor for user item in map fragment
+     * @param uid uid of user represented by the item
+     * @param completeCallback called on completion of the downloading profile photo from
+     *                            firebase to indicate success or failure of the action
+     */
     public MapListItem(String uid, OnCompleteCallback completeCallback) {
         this.uid = uid;
         this.completeCallback = completeCallback;
@@ -31,18 +37,33 @@ public class MapListItem {
         loadFields();
     }
 
+    /**
+     * Returns the profile image of the user represented by the map list item
+     * @return Profile image of the user represented by the map list item
+     */
     public Bitmap getProfileImage() {
         return profileImage;
     }
 
+    /**
+     * Returns the name of the user represented by the map list item
+     * @return the name of the user represented by the map list item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the uid of the user represented by the map list item
+     * @return the uid of the user represented by the map list item
+     */
     public String getUid(){
         return uid;
     }
 
+    /**
+     * Loads the profile image and nickname of the user represented by the map list item
+     */
     protected void loadFields() {
         final int imageSize = 1024 * 1024;
         final byte[] bytes = new byte[imageSize];
@@ -52,11 +73,14 @@ public class MapListItem {
                         @Override
                         public void onSuccess(DocumentSnapshot documentSnapshot) {
                             name = (String) documentSnapshot.get(UserField.NICKNAME.toString());
-                            UserMediaHandler.downloadProfilePhotoFromFirebase(uid, bytes, imageSize, new OnCompleteCallback() {
+                            UserMediaHandler.downloadProfilePhotoFromFirebase(uid, bytes, imageSize,
+                                    new OnCompleteCallback() {
                                         @Override
                                         public void update(boolean success, String message) {
-                                            profileImage = BitmapFactory.decodeByteArray(bytes, 0, imageSize);
-                                            completeCallback.update(true, "Success");
+                                            profileImage = BitmapFactory.decodeByteArray(bytes,
+                                                    0, imageSize);
+                                            completeCallback.update(true,
+                                                    "Success");
                                         }
                                     });
                         }
