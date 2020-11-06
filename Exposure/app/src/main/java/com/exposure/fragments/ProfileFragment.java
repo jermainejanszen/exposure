5da7e148a3956c30a575c14d75e42faf129b9665
Jermaine Janszen <jermainejanszen@gmail.com>
2020-11-06 21:09:46 +1100

    removed unused commits

--- Exposure/app/src/main/java/com/exposure/fragments/ProfileFragment.java
+++ Exposure/app/src/main/java/com/exposure/fragments/ProfileFragment.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
